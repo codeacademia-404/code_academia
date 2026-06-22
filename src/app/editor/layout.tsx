b5123ead7e162cd579dbca3448ca7bdfcf613679
Sasidharan-tech <sasiuzumaki6@gmail.com>
2026-06-22 07:21:11 +0530

    new_files

--- src/app/editor/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/editor.css";
-
-export default function EditorLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <>{children}</>;
-}
--- src/app/learn/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/learn.css";
-
-export default function LearnLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <>{children}</>;
-}
